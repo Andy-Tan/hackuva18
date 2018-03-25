2522ca933fbbd84efaa87924300801459ba4df00
mchltang <mchl.tang@gmail.com>
2018-03-25 01:19:53 -0400

    lol

--- public/scripts/src/main.js
+++ public/scripts/src/main.js
@@ -13,23 +13,6 @@ $(function(){
   const log = console.log
   const logErr = console.error
   const logWarn = console.warn
-  
-  // Read dictionaries from local
-  const stems = function(){$.getJSON("stems.json", function( data ) {
-    return data;
-  })};
-  const stopwords = function(){$.getJSON("stopwords.json", function( data ) {
-    return data;
-  })};
-  const police = function(){$.getJSON("police.json", function( data ) {
-    return data;
-  })};
-  const fire = function(){$.getJSON("fire.json", function( data ) {
-    return data;
-  })};
-  const medical = function(){$.getJSON("medical.json", function( data ) {
-    return data;
-  })};
 
   let state = new Map
   const setState = (key, val, verbose = false) => {
